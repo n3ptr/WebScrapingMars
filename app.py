57c178408af362bd0c6a6980ef5f637fb7faa25e
Nich <nichbern@gmail.com>
2019-09-01 16:34:26 -0500

    fixing pandas df to html

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import scraper_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Mars_app"
 mongo = PyMongo(app)
 
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
 
 @app.route("/")
 def index():
